820d6df0ccf8585221e7fa209c79c6dd19c0c25b
Jakub Szczygiel <kubszczygiel@gmail.com>
2018-12-04 19:43:51 +0100

    reformat

--- BufferedReader/src/Program.java
+++ BufferedReader/src/Program.java
@@ -1,38 +1,35 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Program {
 
     public static void main(String[] args) {
-        int total=0;
-        BufferedReader bufferedReader=null;
+        int total = 0;
+        BufferedReader bufferedReader = null;
 
         try {
-            String line=null;
+            String line = null;
             bufferedReader = new BufferedReader(new FileReader("C:\\Users\\Jakub\\IdeaProjects\\HW\\Banking_application\\Numbers.txt"));
             while ((line = bufferedReader.readLine()) != null) {
                 total += Integer.valueOf(line);
-                System.out.println("Total: " + total);
             }
             System.out.println("Total: " + total);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Opening or Reading from file didn't go well");
-        }
-        finally {
+        } catch (NumberFormatException e) {
+            System.out.println("Your file should contain only numbers!");
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found");
+        } catch (IOException e) {
+            System.out.println("IO problem occured");
+        } finally {
             try {
                 if (bufferedReader != null) {
                     bufferedReader.close();
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Closing file didn't go well");
-
             }
-
         }
-
     }
 }
