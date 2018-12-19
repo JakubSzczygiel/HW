d38b5f10dfc98e9bbfb71cf906767517f79a3a52
Jakub Szczygiel <kubszczygiel@gmail.com>
2018-12-19 20:58:42 +0100

    reformat

---
+++ ReadFromUrl/src/Program.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Program {
+    public static void main(String[] args) {
+        Reader reader=new Reader();
+
+        try {
+            InputStream url = new URL("https://pl.wikipedia.org/wiki/Polska").openStream();
+           // System.out.println("Method 1: "+ reader.getNumberOfStringOccuranceMethod1("Polska",url));
+           // System.out.println("Method 2: "+ reader.getNumberOfStringOccuranceMethod2("Polska",url));
+            System.out.println("Method 3: "+ reader.getNumberOfStringOccuranceMethod3("Polska",url));
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed url exception");
+        } catch (IOException e) {
+            System.out.println("IO exception");
+        } catch (Exception e) {
+            System.out.println("smth go wrong");
+        }
+
+    }
+
+
+}
---
+++ ReadFromUrl/src/Reader.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Reader {
+
+    public int getNumberOfStringOccuranceMethod1(String soughtString, InputStream url) throws IOException {
+        int numberOfOccurances = 0;
+        byte[] buffer = new byte[1000];
+        int count;
+        StringBuilder textFromUrl = new StringBuilder();
+        while ((count = url.read(buffer)) != -1) {
+            textFromUrl.append(new String(buffer, 0, count));
+        }
+        // System.out.println(textFromUrl.toString());
+        numberOfOccurances = textFromUrl.toString().split(soughtString).length - 1;
+        ;
+        return numberOfOccurances;
+    }
+
+
+    public int getNumberOfStringOccuranceMethod2(String soughtString, InputStream url) throws IOException {
+        int numberOfOccurances = 0;
+        byte[] buffer = new byte[10000];
+        int count;
+        while ((count = url.read(buffer)) != -1) {
+            numberOfOccurances += new String(buffer, 0, count).split(soughtString).length - 1;
+        }
+        System.out.println(numberOfOccurances);
+        return numberOfOccurances;
+    }
+
+    public int getNumberOfStringOccuranceMethod3(String soughtString, InputStream url) throws IOException {
+        int numberOfOccurances = 0;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url));
+        String inputLine;
+        while ((inputLine = bufferedReader.readLine()) != null) {
+           // System.out.println(inputLine);
+            numberOfOccurances += inputLine.split(soughtString).length - 1;
+        }
+        bufferedReader.close();
+        return numberOfOccurances;
+    }
+}
